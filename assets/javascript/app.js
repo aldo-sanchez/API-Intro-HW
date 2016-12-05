cdcf8de61775168d7ea87cfa86e39c3e0d915c1d
Aldo Sanchez <Aldo Sanchez>
2016-12-04 18:11:13 -0600

    :construction: Display gifImages in individual columns and rows

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -58,6 +58,7 @@ $('#addButton').on('click', function(){
 });
 
 $(document).on('click', '.gifButton', function(){
+  $('.gifCol').empty();
   var selectedTopic = $(this).attr('id');
   selectedTopic = topics[parseInt(selectedTopic.substring(6))];
   selectedTopic = selectedTopic.split(' ').join('+');
@@ -65,6 +66,8 @@ $(document).on('click', '.gifButton', function(){
 
   var apiKey = 'dc6zaTOxFJmzC';
   var limit = 10;
+
+
   var queryURL = 'http://api.giphy.com/v1/gifs/search?q=' + selectedTopic + '&api_key=' + apiKey + '&limit=' + limit;
   console.log(queryURL);
 
@@ -73,7 +76,64 @@ $(document).on('click', '.gifButton', function(){
     method: 'GET'
   })
   .done(function(response){
+    var results = response.data;
 
+    var numRows = limit/3;
+    if (limit % 3 > 0){
+      numRows = Math.floor(numRows)+1;
+    }
+
+    k = 0;
+  loop1:
+    for (j = 0; j < numRows; j++){
+      console.log("did this")
+      var gifRow = $('<div></div>');
+      gifRow.attr({
+        class: "row",
+        id: 'gifRow' + j
+      });
+      gifRow.appendTo('.gifCol');
+    loop2:
+      for (i = 1; i < results.length; i++){
+        if (i % 3 != 0){
+          i = k;
+          console.log(i)
+          // console.log(k)
+          var rating = results[i].rating;
+          var dataStill = results[i].images.fixed_width_still.url;
+          var dataAnimate = results[i].images.fixed_width.url;
+          var dataState = 'still';
+
+          var ratingText = $('<h3></h3>');
+          ratingText.attr({
+            class: 'gifRating',
+            id: 'gifRating'+ i
+          });
+
+          var gifImage = $('<img>');
+          gifImage.attr({
+            src: dataStill,
+            'data-still': dataStill,
+            'data-animate': dataAnimate,
+            'data-state': dataState
+          });
+
+          var gifDiv = $('<div></div>');
+          gifDiv.attr({
+            class: 'col m4 gif',
+            id: 'gif'+ i
+          });
+          
+          gifDiv.appendTo(gifRow);
+          gifImage.appendTo(gifDiv);
+          }
+        else{
+          console.log("continue", k);
+          break loop2;          
+        }
+          k++;
+      }
+    }
   });
 });
 
